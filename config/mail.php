8dcb1d05ba593821b113c50ca475b69ee29d40df
ZiphoDanca <zipho.dancah@gmail.com>
2019-06-12 09:28:22 +0200

    email setup

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'sendmail'),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => 'zipho.dancah@gmail.com',
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => 'Ezingolweni@4260',
 
     /*
     |--------------------------------------------------------------------------
